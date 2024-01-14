c358655c13428333e71126b6c21ef8ce3696cbdb
Miguel Santiago <miguelsantiago1999@javerianacali.edu.co>
2024-01-14 18:19:35 +0100

    [UPDATE] Se termina la parte inicial del trabajo. Se cargan todas las imagenes y las actualizaciones de los Scripts.

--- T_Final/Scripts/computacion.R
+++ T_Final/Scripts/computacion.R
@@ -1,2 +1,13 @@
 # Coeficiente de asimetria
 
+asim <- function(x){
+  return(mean((x - mean(x))^3)/(sd(x)^3))
+}
+
+
+# Matriz de covarianza
+
+mcov <- function(X){
+  n <- dim(X)[1]
+  return(cov(X)*(n - 1)/n)
+}
